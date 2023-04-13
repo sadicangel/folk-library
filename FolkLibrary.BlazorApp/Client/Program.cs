bcecce15c9ab43db54c54ab6c2f4f9aaaa9f9fc4
sadicangel <sadicangel@hotmail.com>
2023-04-13 01:20:50 +0100

    Adds MudBlazor library and improves existing components.

--- FolkLibrary.BlazorApp/Client/Program.cs
+++ FolkLibrary.BlazorApp/Client/Program.cs
@@ -2,10 +2,13 @@ using FolkLibrary;
 using FolkLibrary.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddMudServices();
 
 builder.Services.AddApplication();
 
